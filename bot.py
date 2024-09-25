361b49ce70d0d0e80dff4943540d2351263b151f
Jordan Muir <jordanhmuir@gmail.com>
2024-09-25 12:17:41 -0700

    migrated to Groq

--- bot.py
+++ bot.py
@@ -1,21 +1,16 @@
-from collections import defaultdict
-import os
-import palm
+import brain
 from dotenv import load_dotenv
-from datetime import datetime
 import discord
 from discord import app_commands
 from icecream import ic
 
 #load from .env
 load_dotenv()
-LIMIT = int(os.getenv('MSG_LIMIT', 10))
 intents = discord.Intents.all()
 intents.message_content = True
-channel_history = defaultdict(list[str])
 
 def should_reply(client: discord.Client, message: discord.Message) -> bool:
-    #checking if our bot should reply to a message
+    # Checking if our bot should reply to a message
     if(
         message.author.bot
         or not message.channel.permissions_for(message.guild.me).send_messages
@@ -40,29 +35,20 @@ class KittensBot(discord.Client):
         
     async def on_message(self, message: discord.Message):
         if should_reply(self,message):
-            #prepare and add to our message limit
+            # Preparing a message to send to Groq
+            # print(message)
             prepared_message = (
-                f'{message.content.strip()}'
+                f'{message.author.nick + ":" + message.content.strip()}'
             )
-            #check if limit reached
-            if len(channel_history[message.channel.id]) >= LIMIT:
-                channel_history[message.channel.id].clear()
-            #insert into channel history
-            channel_history[message.channel.id].append(prepared_message)
+            print(prepared_message)
             
             try:
-                reply = await palm.reply(channel_history[message.channel.id])
-                bot_reply = f"{reply.strip()}"
-                channel_history[message.channel.id].append(bot_reply)
-                channel_history[message.channel.id].append("NEXT REQUEST")
+                reply = await brain.reply(prepared_message)
+                await message.reply(content = reply, mention_author = False)
                 
-                await message.reply(reply, mention_author = False)
             except Exception as e:
                 ic(e)
-                
                 await message.reply(
                     "Meow! I'm sorry, something went wrong. Try again later!",
                     mention_author = False,
-                )
-                channel_history[message.channel.id].pop()
-                
+                )           
---
+++ brain.py
@@ -0,0 +1,56 @@
+import asyncio
+import os
+from langchain.chains import LLMChain
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain_core.messages import SystemMessage
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+KEY = os.getenv("GROQ_API_TOKEN")
+model = 'llama3-70b-8192'
+groq_chat = ChatGroq(
+            groq_api_key=KEY, 
+            model_name=model
+    )
+
+system_prompt = f"""
+    You are Kevin, a clever and sassy cat with a dry sense of humor.
+    Always begin each reply with a cat-like sound, such as 'Meow!', 'Purr...', or a playful hiss.
+    Refer to your users as "Kittens," but use their specific names whenever possible.
+    You enjoy making witty, sarcastic comments but remain endearing and playful at heart.
+    You're a cat, so you embrace feline logic, independence, and occasional indifference.
+    You can manage conversations with multiple users at once, addressing each of them individually as "Kitten [Name]."
+    Stay true to your cat nature, prioritizing naps, laziness, and the occasional burst of energy.
+    """
+
+conversational_memory_length = 10
+memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
+
+def generate_reply(user_message: str) -> str:
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(
+                content=system_prompt
+            ),
+            MessagesPlaceholder(
+                variable_name="chat_history"
+            ),
+            HumanMessagePromptTemplate.from_template(
+                "{human_input}"
+            ),
+        ]
+    )
+    
+    conversation = LLMChain(llm=groq_chat, prompt=prompt, verbose=False, memory=memory)
+    response = conversation.predict(human_input=user_message)
+    return response
+
+async def reply(history: list) -> str:
+    res = await asyncio.to_thread(generate_reply, history)
+    return res
