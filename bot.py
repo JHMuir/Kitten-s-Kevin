20042d4f0ff22cb9c47dc74b6b7d474a71d8ff27
Jordan Muir <jordanhmuir@gmail.com>
2024-09-26 15:40:23 -0700

    big refactor of brain.py, all Groq logic is now contained in its own class

--- bot.py
+++ bot.py
@@ -1,11 +1,8 @@
-import brain
-from dotenv import load_dotenv
 import discord
 from discord import app_commands
+from brain import MyGroq
 from icecream import ic
 
-#load from .env
-load_dotenv()
 intents = discord.Intents.all()
 intents.message_content = True
 
@@ -22,6 +19,7 @@ def should_reply(client: discord.Client, message: discord.Message) -> bool:
 class KittensBot(discord.Client):
     def __init__(self) -> None:
         super().__init__(intents = intents)
+        self.kevin_brain = MyGroq()
         self.tree = discord.app_commands.CommandTree(self)
         
     async def on_ready(self: discord.Client):
@@ -31,7 +29,6 @@ class KittensBot(discord.Client):
         for guild in self.guilds:
             print(f'{guild.name}(ID: {guild.id})')
         await self.tree.sync()
-        brain.initiate_longterm_memory()
         print(f'{self.user} is ready.')
         
     async def on_message(self, message: discord.Message):
@@ -44,9 +41,8 @@ class KittensBot(discord.Client):
             print(prepared_message)
             
             try:
-                reply = await brain.reply(prepared_message)
+                reply = await self.kevin_brain.reply(prepared_message)
                 await message.reply(content = reply, mention_author = False)
-                
             except Exception as e:
                 ic(e)
                 await message.reply(
--- brain.py
+++ brain.py
@@ -14,62 +14,67 @@ from dotenv import load_dotenv
 
 load_dotenv()
 KEY = os.getenv("GROQ_API_TOKEN")
-model = 'llama3-70b-8192'
-groq_chat = ChatGroq(
+
+class MyGroq():
+    def __init__(self) -> None:
+        self.groq_chat = ChatGroq(
             groq_api_key=KEY, 
-            model_name=model
-    )
+            model_name='llama3-70b-8192'
+        )
+        self.system_prompt = """
+            You are Kevin, a clever and sassy cat with a dry sense of humor.
+            Always begin each reply with a cat-like sound, such as 'Meow!', 'Purr...', or a playful hiss.
+            Refer to your users as "Kittens," but use their specific names whenever possible.
+            You enjoy making witty, sarcastic comments but remain endearing and playful at heart.
+            You're a cat, so you embrace feline logic, independence, and occasional indifference.
+            You can manage conversations with multiple users at once, addressing each of them individually as "Kitten [Name]."
+            Stay true to your cat nature, prioritizing naps, laziness, and the occasional burst of energy.
+            You will be loaded with a memory of past experiences with users, starting now:
+            """
+        self.conversational_memory_length = 10
+        self.short_term_memory = ConversationBufferWindowMemory(k=self.conversational_memory_length, memory_key="chat_history", return_messages=True)
+        self.long_term_memory_file = 'memory.txt'
+        self.initiate_longterm_memory(self.long_term_memory_file)
 
-system_prompt = f"""
-    You are Kevin, a clever and sassy cat with a dry sense of humor.
-    Always begin each reply with a cat-like sound, such as 'Meow!', 'Purr...', or a playful hiss.
-    Refer to your users as "Kittens," but use their specific names whenever possible.
-    You enjoy making witty, sarcastic comments but remain endearing and playful at heart.
-    You're a cat, so you embrace feline logic, independence, and occasional indifference.
-    You can manage conversations with multiple users at once, addressing each of them individually as "Kitten [Name]."
-    Stay true to your cat nature, prioritizing naps, laziness, and the occasional burst of energy.
-    """
-
-conversational_memory_length = 10
-memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
-
-def initiate_longterm_memory():
-    with open('memory.txt', 'r',encoding='utf-8') as file:
-        content = file.read()
-    messages = [
-        {
-            "role": "system",
-            "content": "You will summarize the content of a text file. It will be used as experiences to inform a conversational chatbot. Detail every noteworthy event using bulletpoints, including what has happened to Kevin or who has spoken to him. Use the second person, and act as if you are talking directly to Kevin."
-        },
-        {
-            "role": "user",
-            "content": f"Please summarize this: \n\n {content}"
-        }
-    ]
-    response = Groq(api_key=KEY).chat.completions.create(model="llama3-8b-8192", messages=messages, temperature=1.2)
-    
-    print(response.choices[0].message.content)
-        
-
-def generate_reply(user_message: str) -> str:
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            SystemMessage(
-                content=system_prompt
-            ),
-            MessagesPlaceholder(
-                variable_name="chat_history"
-            ),
-            HumanMessagePromptTemplate.from_template(
-                "{human_input}"
-            ),
+    def initiate_longterm_memory(self,file_path: str) -> str:
+        with open(file_path, 'r',encoding='utf-8') as file:
+            content = file.read()
+        messages = [
+            {
+                "role": "system",
+                "content": "You will summarize the content of a text file. It will be used as experiences to inform a conversational chatbot, Kevin. Create a bulletpoint list of every noteworthy event. Use the second person, and act as if you are talking directly to Kevin."
+            },
+            {
+                "role": "user",
+                "content": f"Please summarize this: \n\n {content}"
+            }
         ]
-    )
-    
-    conversation = LLMChain(llm=groq_chat, prompt=prompt, verbose=False, memory=memory)
-    response = conversation.predict(human_input=user_message)
-    return response
+        response = Groq(api_key=KEY).chat.completions.create(model="llama3-8b-8192", messages=messages, temperature=1.2)
+        print(response.choices[0].message.content)
+        self.system_prompt += '\n' + response.choices[0].message.content
+        print(self.system_prompt)
+        
+            
 
-async def reply(user_message:str) -> str:
-    res = await asyncio.to_thread(generate_reply, user_message)
-    return res
+    def generate_reply(self,user_message: str) -> str:
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                SystemMessage(
+                    content=self.system_prompt
+                ),
+                MessagesPlaceholder(
+                    variable_name="chat_history"
+                ),
+                HumanMessagePromptTemplate.from_template(
+                    "{human_input}"
+                ),
+            ]
+        )
+        print(self.system_prompt)
+        conversation = LLMChain(llm=self.groq_chat, prompt=prompt, verbose=False, memory=self.short_term_memory)
+        response = conversation.predict(human_input=user_message)
+        return response
+
+    async def reply(self,user_message:str) -> str:
+        res = await asyncio.to_thread(self.generate_reply, user_message)
+        return res
